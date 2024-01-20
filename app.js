040666922fe5d4fe82e9c7afb89dd5531bd148c8
gabrielkaique19 <gabrielkaiquec@gmail.com>
2024-01-20 02:52:25 -0300

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 function exibirMensagemInicial() {
-    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('h1', 'Jogo do número secreto Feito Por: Gabriel Kaique');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
 
